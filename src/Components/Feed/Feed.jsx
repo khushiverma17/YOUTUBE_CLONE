d4077c9401a2f7910f490a71c623c1e1f07eed07
khushi <khushiverma1708@gmail.com>
2024-02-19 01:09:20 +0530

    19feb

--- src/Components/Feed/Feed.jsx
+++ src/Components/Feed/Feed.jsx
@@ -61,6 +61,48 @@ const Feed=()=>{
                 <h3>My channel</h3>
                 <p>15k views &bull; 2 days ago</p>
             </div>
+            <div className="card">
+                <img src="https://media.istockphoto.com/id/1317323736/photo/a-view-up-into-the-trees-direction-sky.jpg?s=612x612&w=0&k=20&c=i4HYO7xhao7CkGy7Zc_8XSNX_iqG0vAwNsrH1ERmw2Q=" alt="" />
+                <h2>Best channel</h2>
+                {/* channel name */}
+                <h3>My channel</h3>
+                <p>15k views &bull; 2 days ago</p>
+            </div>
+            <div className="card">
+                <img src="https://media.istockphoto.com/id/1317323736/photo/a-view-up-into-the-trees-direction-sky.jpg?s=612x612&w=0&k=20&c=i4HYO7xhao7CkGy7Zc_8XSNX_iqG0vAwNsrH1ERmw2Q=" alt="" />
+                <h2>Best channel</h2>
+                {/* channel name */}
+                <h3>My channel</h3>
+                <p>15k views &bull; 2 days ago</p>
+            </div>
+            <div className="card">
+                <img src="https://media.istockphoto.com/id/1317323736/photo/a-view-up-into-the-trees-direction-sky.jpg?s=612x612&w=0&k=20&c=i4HYO7xhao7CkGy7Zc_8XSNX_iqG0vAwNsrH1ERmw2Q=" alt="" />
+                <h2>Best channel</h2>
+                {/* channel name */}
+                <h3>My channel</h3>
+                <p>15k views &bull; 2 days ago</p>
+            </div>
+            <div className="card">
+                <img src="https://media.istockphoto.com/id/1317323736/photo/a-view-up-into-the-trees-direction-sky.jpg?s=612x612&w=0&k=20&c=i4HYO7xhao7CkGy7Zc_8XSNX_iqG0vAwNsrH1ERmw2Q=" alt="" />
+                <h2>Best channel</h2>
+                {/* channel name */}
+                <h3>My channel</h3>
+                <p>15k views &bull; 2 days ago</p>
+            </div>
+            <div className="card">
+                <img src="https://media.istockphoto.com/id/1317323736/photo/a-view-up-into-the-trees-direction-sky.jpg?s=612x612&w=0&k=20&c=i4HYO7xhao7CkGy7Zc_8XSNX_iqG0vAwNsrH1ERmw2Q=" alt="" />
+                <h2>Best channel</h2>
+                {/* channel name */}
+                <h3>My channel</h3>
+                <p>15k views &bull; 2 days ago</p>
+            </div>
+            <div className="card">
+                <img src="https://media.istockphoto.com/id/1317323736/photo/a-view-up-into-the-trees-direction-sky.jpg?s=612x612&w=0&k=20&c=i4HYO7xhao7CkGy7Zc_8XSNX_iqG0vAwNsrH1ERmw2Q=" alt="" />
+                <h2>Best channel</h2>
+                {/* channel name */}
+                <h3>My channel</h3>
+                <p>15k views &bull; 2 days ago</p>
+            </div>
         </div>
     )
 }
