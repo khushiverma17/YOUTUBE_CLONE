53d4cef65beec208c89bdcecd265ce3c6f4d0e38
khushi <khushiverma1708@gmail.com>
2024-02-19 01:02:44 +0530

    19 feb

--- src/Pages/Home/Home.jsx
+++ src/Pages/Home/Home.jsx
@@ -1,11 +1,15 @@
 import React from 'react'
 import Sidebar from '../../Components/Sidebar/Sidebar'
 import './Home.css'
+import Feed from '../../Components/Feed/Feed'
 
 const Home=({sidebar})=>{
     return (
         <>
             <Sidebar sidebar={sidebar}/>
+            <div className={`container ${sidebar?"":'large-container'}`}>
+                <Feed/>
+            </div>
         </>
     )
 }
