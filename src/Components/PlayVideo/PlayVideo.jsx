d4e199da5354818b12108efbe6d0a258308dbfae
khushi <khushiverma1708@gmail.com>
2024-04-17 17:10:19 +0530

    fetching and updating data on ui

--- src/Components/PlayVideo/PlayVideo.jsx
+++ src/Components/PlayVideo/PlayVideo.jsx
@@ -1,87 +1,95 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 import './PlayVideo.css'
-const PlayVideo=({videoId})=>{
+import { valueConvertor } from "../../data";
+import moment from "moment";
+import { useParams } from "react-router-dom";
+const API_KEY = import.meta.env.VITE_API_KEY;
+
+
+const PlayVideo = () => {
+    const {videoId} = useParams();
+    const [apiData, setApiData] = useState(null);
+    const [channelData, setChannelData] = useState(null);
+    const [commentData, setCommentData] = useState([]);
 
 
 
-    return(
+    const fetchVideoData = async () => {
+        // we can directly pass url without storing it in a variable
+        //fetching videos data
+        const videoDetails_url = `https://youtube.googleapis.com/youtube/v3/videos?part=snippet%2CcontentDetails%2Cstatistics&id=${videoId}&key=${API_KEY}`
+        await fetch(videoDetails_url).then(res => res.json()).then(data => setApiData(data.items[0]))
+    }
+
+    const fetchOtherData = async () => {
+        //fetching channel data
+        const channelData_url = `https://youtube.googleapis.com/youtube/v3/channels?part=snippet%2CcontentDetails%2Cstatistics&id=${apiData.snippet.channelId}&key=${API_KEY}`
+        await fetch(channelData_url).then(res => res.json()).then(data => setChannelData(data.items[0]))
+
+        //fetching comments data
+        const comment_url = `https://youtube.googleapis.com/youtube/v3/commentThreads?part=snippet%2C%20replies&maxResults=50&videoId=${videoId}&key=${API_KEY}`
+        await fetch(comment_url).then(res => res.json()).then(data => setCommentData(data.items))
+
+    }
+
+    useEffect(() => {
+        fetchVideoData();
+    }, [videoId])
+
+    // fetchVideoData should executed first than fetchOtherData
+
+    useEffect(() => {
+        fetchOtherData();
+    }, [apiData])
+
+
+    return (
         <div className="play-video">
             {/* width="560" height="315"  */}
-            <iframe width="560" height="315" src={`https://www.youtube.com/embed/${videoId}?autoplay=1`}  frameborder="0" allow="accelerometer; clipboard-write; encrypted-media; gyroscope; autoplay; picture-in-picture; web-share" allowfullscreen></iframe>
-            <h3>Best youtube channel you have landed on</h3>
+            <iframe width="560" height="315" src={`https://www.youtube.com/embed/${videoId}?autoplay=1`} frameborder="0" allow="accelerometer; clipboard-write; encrypted-media; gyroscope; autoplay; picture-in-picture; web-share" allowfullscreen></iframe>
+            <h3>{apiData ? apiData.snippet.title : "Title Here"}</h3>
+            <br />
+            <p>{apiData ? valueConvertor(apiData.statistics.viewCount) : "16K"} views &bull; {apiData ? moment(apiData.snippet.publishedAt).fromNow() : "2"}</p>
             <hr />
             <div className="publisher">
-            
-                <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQnCsYj8-Oj5u-SgEQIU9sQJyqpF76n335Bhjr3J8Bg_g&s" alt="" />
+
+                <img src={channelData ? channelData.snippet.thumbnails.default.url : ""} alt="" />
                 <div id="channelinfo">
-                    <p>Great Video</p>
-                    <span>1 M Subscribers</span>
+                    <p>{apiData ? apiData.snippet.channelTitle : "Channel Name here"}</p>
+                    <span>{channelData ? valueConvertor(channelData.statistics.subscriberCount) : "1M"} Subscribers</span>
                 </div>
                 <button>Subscribe</button>
                 <div id="channelinforeact">
-                    <span><i className="material-icons video-material-icons">thumb_up</i><span class="hit-react">234</span></span>
-                    <span><i className="material-icons video-material-icons">thumb_down</i><span class="hit-react">23</span></span>
-                    <span><i className="material-icons video-material-icons">share</i><span class="hit-react">Like</span></span>
-                    <span><i className="material-icons video-material-icons">download</i><span class="hit-react">Dislike</span></span>
+                    <span><i className="material-icons video-material-icons">thumb_up</i><span class="hit-react">{apiData ? valueConvertor(apiData.statistics.likeCount) : "20"}</span></span>
+                    {" "}
+                    <span><i className="material-icons video-material-icons">thumb_down</i></span>
+                    <span><i className="material-icons video-material-icons">share</i></span>
+                    <span><i className="material-icons video-material-icons">download</i></span>
                 </div>
             </div>
             <div className="vid-description">
-                <p>Channel that makes learning easy</p>
-                <p>Subscribe to my channel</p>
+                <p>{apiData ? apiData.snippet.localized.description.slice(0, 250) : "Description here"}</p>
+
                 <hr />
-                <h4>130 comments</h4>
-                <div className="comment">
-                    <img src="https://i.pinimg.com/236x/3e/a9/47/3ea947c7c2ae57d763a9442fee8f1f2a.jpg" alt="" />
-                    <div>
-                        <h3>Jocky<span>1 day ago</span></h3>
-                        <p>The video provides us a great learingj elsdf Lorem ipsum dolor sit amet.</p>
-                        <div className="comment-action">
-                            <i className="material-icons">thumb_up</i>
-                            <span>456</span>
-                            <i className="material-icons">thumb_down</i>
-                            <span>67</span>
+                <h4>{apiData ? valueConvertor(apiData.statistics.commentCount) : "10"} comments</h4>
+                {commentData.map((item, index) => {
+                    return (
+                        <div key={index} className="comment">
+                            <img src={item.snippet.topLevelComment.snippet.authorProfileImageUrl} alt="" />
+                            <div>
+                                <h3>{item.snippet.topLevelComment.snippet.authorDisplayName}<span>{moment(item.snippet.topLevelComment.snippet.publishedAt).fromNow()}</span></h3>
+                                <p>{item.snippet.topLevelComment.snippet.textDisplay.slice(0, 300)}</p>
+                                <div className="comment-action">
+                                    <i className="material-icons">thumb_up</i>
+                                    <span className="likecomment">{valueConvertor(item.snippet.topLevelComment.snippet.likeCount)}</span>
+                                    <i className="material-icons">thumb_down</i>
+                                </div>
+                            </div>
                         </div>
-                    </div>
-                </div>
-                <div className="comment">
-                    <img src="https://i.pinimg.com/236x/3e/a9/47/3ea947c7c2ae57d763a9442fee8f1f2a.jpg" alt="" />
-                    <div>
-                        <h3>Jocky<span>1 day ago</span></h3>
-                        <p>The video provides us a great learingj elsdf Lorem ipsum dolor sit amet.</p>
-                        <div className="comment-action">
-                            <i className="material-icons">thumb_up</i>
-                            <span>456</span>
-                            <i className="material-icons">thumb_down</i>
-                            <span>67</span>
-                        </div>
-                    </div>
-                </div>
-                <div className="comment">
-                    <img src="https://i.pinimg.com/236x/3e/a9/47/3ea947c7c2ae57d763a9442fee8f1f2a.jpg" alt="" />
-                    <div>
-                        <h3>Jocky<span>1 day ago</span></h3>
-                        <p>The video provides us a great learingj elsdf Lorem ipsum dolor sit amet.</p>
-                        <div className="comment-action">
-                            <i className="material-icons">thumb_up</i>
-                            <span>456</span>
-                            <i className="material-icons">thumb_down</i>
-                            <span>67</span>
-                        </div>
-                    </div>
-                </div>
-                <div className="comment">
-                    <img src="https://i.pinimg.com/236x/3e/a9/47/3ea947c7c2ae57d763a9442fee8f1f2a.jpg" alt="" />
-                    <div>
-                        <h3>Jocky<span>1 day ago</span></h3>
-                        <p>The video provides us a great learingj elsdf Lorem ipsum dolor sit amet.</p>
-                        <div className="comment-action">
-                            <i className="material-icons">thumb_up</i>
-                            <span>456</span>
-                            <i className="material-icons">thumb_down</i>
-                            <span>67</span>
-                        </div>
-                    </div>
-                </div>
+                    )
+                })}
+
+
             </div>
         </div>
     )
--- src/Components/Recommended/Recommended.jsx
+++ src/Components/Recommended/Recommended.jsx
@@ -1,161 +1,40 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import './Recommended.css';
+import { valueConvertor } from "../../data";
+import { Link } from "react-router-dom";
+const API_KEY = import.meta.env.VITE_API_KEY;
 
-const Recommended=()=>{
-    return(
+const Recommended = ({ categoryId }) => {
+
+    const [apiData, setApiData] = useState([]);
+
+    const fetchData = async () => {
+        const relatedVideo_url = `https://youtube.googleapis.com/youtube/v3/videos?part=snippet%2CcontentDetails%2Cstatistics&chart=mostPopular&maxResults=45&regionCode=US&videoCategoryId=${categoryId}&key=${API_KEY}`
+        await fetch(relatedVideo_url).then(res => res.json()).then(data => setApiData(data.items))
+
+    }
+
+    // When you don't pass any dependencies to the useEffect hook, it means that the effect will run after every render of the component.
+    useEffect(() => {
+        fetchData();
+    }, [])
+
+
+    return (
         <div className="recommended">
-            <div className="side-video-list">
-                <img src="https://cdn.pixabay.com/photo/2023/02/03/05/11/youtube-background-7764170_640.jpg" alt="" />
-                <div className="vid-info">
-                    <h4>Best channel to learn exciting</h4>
-                    <p>Trendy Channel</p>
-                    <p>1239k Views</p>
-                </div>
-            </div>
-            <div className="side-video-list">
-                <img src="https://cdn.pixabay.com/photo/2023/02/03/05/11/youtube-background-7764170_640.jpg" alt="" />
-                <div className="vid-info">
-                    <h4>Best channel to learn exciting</h4>
-                    <p>Trendy Channel</p>
-                    <p>1239k Views</p>
-                </div>
-            </div>
-            <div className="side-video-list">
-                <img src="https://cdn.pixabay.com/photo/2023/02/03/05/11/youtube-background-7764170_640.jpg" alt="" />
-                <div className="vid-info">
-                    <h4>Best channel to learn exciting</h4>
-                    <p>Trendy Channel</p>
-                    <p>1239k Views</p>
-                </div>
-            </div>
-            <div className="side-video-list">
-                <img src="https://cdn.pixabay.com/photo/2023/02/03/05/11/youtube-background-7764170_640.jpg" alt="" />
-                <div className="vid-info">
-                    <h4>Best channel to learn exciting</h4>
-                    <p>Trendy Channel</p>
-                    <p>1239k Views</p>
-                </div>
-            </div>
-            <div className="side-video-list">
-                <img src="https://cdn.pixabay.com/photo/2023/02/03/05/11/youtube-background-7764170_640.jpg" alt="" />
-                <div className="vid-info">
-                    <h4>Best channel to learn exciting</h4>
-                    <p>Trendy Channel</p>
-                    <p>1239k Views</p>
-                </div>
-            </div>
-            <div className="side-video-list">
-                <img src="https://cdn.pixabay.com/photo/2023/02/03/05/11/youtube-background-7764170_640.jpg" alt="" />
-                <div className="vid-info">
-                    <h4>Best channel to learn exciting</h4>
-                    <p>Trendy Channel</p>
-                    <p>1239k Views</p>
-                </div>
-            </div>
-            <div className="side-video-list">
-                <img src="https://cdn.pixabay.com/photo/2023/02/03/05/11/youtube-background-7764170_640.jpg" alt="" />
-                <div className="vid-info">
-                    <h4>Best channel to learn exciting</h4>
-                    <p>Trendy Channel</p>
-                    <p>1239k Views</p>
-                </div>
-            </div>
-            <div className="side-video-list">
-                <img src="https://cdn.pixabay.com/photo/2023/02/03/05/11/youtube-background-7764170_640.jpg" alt="" />
-                <div className="vid-info">
-                    <h4>Best channel to learn exciting</h4>
-                    <p>Trendy Channel</p>
-                    <p>1239k Views</p>
-                </div>
-            </div>
-            <div className="side-video-list">
-                <img src="https://cdn.pixabay.com/photo/2023/02/03/05/11/youtube-background-7764170_640.jpg" alt="" />
-                <div className="vid-info">
-                    <h4>Best channel to learn exciting</h4>
-                    <p>Trendy Channel</p>
-                    <p>1239k Views</p>
-                </div>
-            </div>
-            <div className="side-video-list">
-                <img src="https://cdn.pixabay.com/photo/2023/02/03/05/11/youtube-background-7764170_640.jpg" alt="" />
-                <div className="vid-info">
-                    <h4>Best channel to learn exciting</h4>
-                    <p>Trendy Channel</p>
-                    <p>1239k Views</p>
-                </div>
-            </div>
-            <div className="side-video-list">
-                <img src="https://cdn.pixabay.com/photo/2023/02/03/05/11/youtube-background-7764170_640.jpg" alt="" />
-                <div className="vid-info">
-                    <h4>Best channel to learn exciting</h4>
-                    <p>Trendy Channel</p>
-                    <p>1239k Views</p>
-                </div>
-            </div>
-            <div className="side-video-list">
-                <img src="https://cdn.pixabay.com/photo/2023/02/03/05/11/youtube-background-7764170_640.jpg" alt="" />
-                <div className="vid-info">
-                    <h4>Best channel to learn exciting</h4>
-                    <p>Trendy Channel</p>
-                    <p>1239k Views</p>
-                </div>
-            </div>
-            <div className="side-video-list">
-                <img src="https://cdn.pixabay.com/photo/2023/02/03/05/11/youtube-background-7764170_640.jpg" alt="" />
-                <div className="vid-info">
-                    <h4>Best channel to learn exciting</h4>
-                    <p>Trendy Channel</p>
-                    <p>1239k Views</p>
-                </div>
-            </div>
-            <div className="side-video-list">
-                <img src="https://cdn.pixabay.com/photo/2023/02/03/05/11/youtube-background-7764170_640.jpg" alt="" />
-                <div className="vid-info">
-                    <h4>Best channel to learn exciting</h4>
-                    <p>Trendy Channel</p>
-                    <p>1239k Views</p>
-                </div>
-            </div>
-            <div className="side-video-list">
-                <img src="https://cdn.pixabay.com/photo/2023/02/03/05/11/youtube-background-7764170_640.jpg" alt="" />
-                <div className="vid-info">
-                    <h4>Best channel to learn exciting</h4>
-                    <p>Trendy Channel</p>
-                    <p>1239k Views</p>
-                </div>
-            </div>
-            <div className="side-video-list">
-                <img src="https://cdn.pixabay.com/photo/2023/02/03/05/11/youtube-background-7764170_640.jpg" alt="" />
-                <div className="vid-info">
-                    <h4>Best channel to learn exciting</h4>
-                    <p>Trendy Channel</p>
-                    <p>1239k Views</p>
-                </div>
-            </div>
-            <div className="side-video-list">
-                <img src="https://cdn.pixabay.com/photo/2023/02/03/05/11/youtube-background-7764170_640.jpg" alt="" />
-                <div className="vid-info">
-                    <h4>Best channel to learn exciting</h4>
-                    <p>Trendy Channel</p>
-                    <p>1239k Views</p>
-                </div>
-            </div>
-            <div className="side-video-list">
-                <img src="https://cdn.pixabay.com/photo/2023/02/03/05/11/youtube-background-7764170_640.jpg" alt="" />
-                <div className="vid-info">
-                    <h4>Best channel to learn exciting</h4>
-                    <p>Trendy Channel</p>
-                    <p>1239k Views</p>
-                </div>
-            </div>
-            <div className="side-video-list">
-                <img src="https://cdn.pixabay.com/photo/2023/02/03/05/11/youtube-background-7764170_640.jpg" alt="" />
-                <div className="vid-info">
-                    <h4>Best channel to learn exciting</h4>
-                    <p>Trendy Channel</p>
-                    <p>1239k Views</p>
-                </div>
-            </div>
+            {apiData.map((item, index) => {
+                return (
+                    <Link to={`/video/${item.snippet.categoryId}/${item.id}`} key={index} className="side-video-list">
+                        <img src={item.snippet.thumbnails.medium.url} alt="" />
+                        <div className="vid-info">
+                            <h4 className="sidevideodata">{item.snippet.title}</h4>
+                            <p className="sidevideodata">{item.snippet.channelTitle}</p>
+                            <p className="sidevideodata">{valueConvertor(item.statistics.viewCount)} Views</p>
+                        </div>
+                    </Link>
+                )
+            })}
+
 
         </div>
     )
